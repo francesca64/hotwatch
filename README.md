65d49b0ba3d855c513d6b8c4b6c4dc4b42ed03d9
Francesca Frangipane <francesca@comfysoft.net>
2018-06-15 14:31:16 -0400

    Resurrect from the dead!

--- README.md
+++ README.md
@@ -5,17 +5,23 @@
 
 [Documentation](https://francesca64.github.io/hotwatch/docs/hotwatch)
 
-*hotwatch* is a Rust library for comfortably watching and handling file changes. It's a thin convenience wrapper over [notify](https://github.com/passcod/notify), allowing you to easily spawn handlers.
+`hotwatch` is a Rust library for comfortably watching and handling file changes. It's a thin convenience wrapper over [notify](https://github.com/passcod/notify), allowing you to easily spawn handlers.
 
-Nightly Rust is required, since I used the box keyword a few times. Sorry.
+At least Rust 1.24 is required, due to the requirements of [`parking_lot`](https://github.com/Amanieu/parking_lot).
 
 ```rust
 use hotwatch::{Hotwatch, Event};
 
 let mut hotwatch = Hotwatch::new().expect("Hotwatch failed to initialize.");
-hotwatch.watch("war.png", |e: Event| {
-    if let Event::Write(path) = e {
+hotwatch.watch("war.png", |event: Event| {
+    if let Event::Write(path) = event {
         println!("War has changed.");
     }
 }).expect("Failed to watch file!");
 ```
+
+## Why should I use this instead of [`warmy`](https://github.com/phaazon/warmy)?
+
+`warmy` is a more general solution for responding to resource changes. `hotwatch` is very simplistic and intends to be trivial to integrate.
+
+I've never actually used `warmy`, though. It's probably awesome. I just know that `hotwatch` is really easy to use, and has a sexier name.
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
-//! hotwatch is a Rust library for comfortably watching and handling file changes.
-//! It's a thin convenience wrapper over [notify](https://github.com/passcod/notify), allowing you to easily spawn handlers.
+//! `hotwatch` is a Rust library for comfortably watching and handling file changes.
+//! It's a thin convenience wrapper over [notify](https://github.com/passcod/notify),
+//! allowing you to easily spawn handlers.
 //!
 //! Watching is done on a separate thread to avoid blocking your enjoyment of life.
 //! All handlers are run on that thread as well, so keep that in mind when attempting to access
 //! outside data from within a handler.
 //!
-//! Nightly Rust is required, as hotwatch uses the box keyword internally.
-
-#![feature(box_syntax)]
+//! At least Rust 1.24 is required, due to the requirements of [`parking_lot`](https://github.com/Amanieu/parking_lot).
 
 #[macro_use] extern crate derive_more;
+#[macro_use] extern crate log;
 extern crate notify;
 extern crate parking_lot;
 
 use std::collections::HashMap;
-use std::path::{Path, PathBuf};
 use std::fs::canonicalize;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::sync::mpsc::{channel, Receiver};
 use std::time::Duration;
@@ -27,21 +27,21 @@ pub use notify::DebouncedEvent as Event;
 
 fn path_from_event(e: &Event) -> Option<PathBuf> {
     match e {
-        &Event::NoticeWrite(ref p) |
-        &Event::NoticeRemove(ref p) |
-        &Event::Create(ref p) |
-        &Event::Write(ref p) |
-        &Event::Chmod(ref p) |
-        &Event::Remove(ref p) |
-        &Event::Rename(ref p, _) => Some(p.clone()),
-        &_ => None
+        &Event::NoticeWrite(ref p)
+        | &Event::NoticeRemove(ref p)
+        | &Event::Create(ref p)
+        | &Event::Write(ref p)
+        | &Event::Chmod(ref p)
+        | &Event::Remove(ref p)
+        | &Event::Rename(ref p, _) => Some(p.clone()),
+        &_ => None,
     }
 }
 
 #[derive(Debug, From)]
 pub enum Error {
     Io(std::io::Error),
-    Notify(notify::Error)
+    Notify(notify::Error),
 }
 
 type HotwatchResult<T> = Result<T, Error>;
@@ -59,7 +59,7 @@ impl Hotwatch {
     ///
     /// # Errors
     ///
-    /// This function can fail if the underlying [notify](https://docs.rs/notify/4.0.1/notify/)
+    /// This function can fail if the underlying [notify](https://docs.rs/notify/4.0.3/notify/)
     /// instance fails to initialize. This will unfortunately expose you to notify's own error
     /// type; hotwatch doesn't perfectly encapsulate this.
     ///
@@ -72,24 +72,21 @@ impl Hotwatch {
     /// ```
     pub fn new() -> HotwatchResult<Self> {
         let (tx, rx) = channel();
-        let handler_map_mutex = Arc::new(Mutex::new(HashMap::new()));
+        let handler_map_mutex: Arc<Mutex<HashMap<_, _>>> = Default::default();
         Hotwatch::run(handler_map_mutex.clone(), rx);
         Watcher::new(tx, Duration::from_secs(2))
             .map_err(Error::Notify)
-            .map(|watcher| Hotwatch {
-                watcher: watcher,
-                handler_map_mutex: handler_map_mutex,
-            })
+            .map(|watcher| Hotwatch { watcher, handler_map_mutex })
     }
 
     /// Watch a path and register a handler to it.
     ///
     /// When watching a directory, that handler will receive all events for all directory
-    /// contents, even recursiving through subdirectories.
+    /// contents, even recursing through subdirectories.
     ///
     /// Only the most specific applicable handler will be called. In other words, if you're
     /// watching "dir" and "dir/file1", then only the latter handler will fire for changes to
-    /// file1.
+    /// `file1`.
     ///
     /// Note that handlers will be run in hotwatch's watch thread, so you'll have to use `move`
     /// if the closure captures anything.
@@ -105,26 +102,25 @@ impl Hotwatch {
     /// use hotwatch::{Hotwatch, Event};
     ///
     /// let mut hotwatch = Hotwatch::new().expect("Hotwatch failed to initialize.");
-    /// hotwatch.watch("README.md", |e: Event| {
-    ///     if let Event::Write(path) = e {
+    /// hotwatch.watch("README.md", |event: Event| {
+    ///     if let Event::Write(path) = event {
     ///         println!("{:?} changed!", path);
     ///     }
     /// }).expect("Failed to watch file!");
     /// ```
     pub fn watch<P, F>(&mut self, path: P, handler: F) -> HotwatchResult<()>
-    where P: AsRef<Path>, F: 'static + Fn(Event) + Send {
+        where P: AsRef<Path>, F: 'static + Fn(Event) + Send
+    {
         let absolute_path = canonicalize(path)?;
         let mut handlers = self.handler_map_mutex.lock();
         self.watcher.watch(Path::new(&absolute_path), RecursiveMode::Recursive)
-            .map_err(|e| match e {
-                notify::Error::Io(e) => Error::Io(e),
-                _ => Error::Notify(e)
+            .map_err(|err| match err {
+                notify::Error::Io(err) => Error::Io(err),
+                _ => Error::Notify(err),
             })
             .map(|_| {
-                (*handlers).insert(PathBuf::from(absolute_path), box handler);
-                if cfg!(debug_assertions) {
-                    println!("HotwatchHandlers {:?}", handlers.keys());
-                }
+                (*handlers).insert(PathBuf::from(absolute_path), Box::new(handler));
+                debug!("HotwatchHandlers {:?}", handlers.keys());
             })
     }
 
@@ -133,20 +129,13 @@ impl Hotwatch {
             loop {
                 match rx.recv() {
                     Ok(event) => {
-                        if cfg!(debug_assertions) {
-                            println!("HotwatchEvent {:?}", event);
-                        }
+                        debug!("HotwatchEvent {:?}", event);
                         let handlers = handler_map_mutex.lock();
                         if let Some(mut path) = path_from_event(&event) {
                             let mut handler = None;
                             let mut poppable = true;
-                            if cfg!(debug_assertions) {
-                                println!("HotwatchMatch");
-                            }
                             while handler.is_none() && poppable {
-                                if cfg!(debug_assertions) {
-                                    println!(" -> {:?}", path);
-                                }
+                                debug!("HotwatchMatch {:?}", path);
                                 handler = (*handlers).get(&path);
                                 poppable = path.pop();
                             }
@@ -156,9 +145,7 @@ impl Hotwatch {
                         }
                     },
                     Err(e) => {
-                        if cfg!(debug_assertions) {
-                            println!("Receiver error: {:?}", e);
-                        }
+                        error!("Receiver error: {:?}", e);
                     }
                 }
             }
