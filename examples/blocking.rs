4ebc3424b6b5a6af7742a9b597c9b1ddb639e666
Juan Eugenio Abadie <juaneabadie@gmail.com>
2020-04-11 23:11:50 -0300

    Use known enum

--- examples/blocking.rs
+++ examples/blocking.rs
@@ -1,10 +1,11 @@
 use hotwatch::{
     blocking::{Flow, Hotwatch},
     Event,
+    Error,
 };
 use std::path::Path;
 
-fn main() -> Result<(), failure::Error> {
+fn main() -> Result<(), Error> {
     let mut watcher = Hotwatch::new()?;
     let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("examples/data.json");
     watcher.watch(&path, move |event| {
